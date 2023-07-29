158740d1d52bcc28004c2fc8d618a4cc5932f603
Nikhil Rathod <rathod.nick0509@gmail.com>
2023-07-29 22:23:50 +0530

    docs: project complete
    random joke generator

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function generate(){
+const jokes = ['What do you call a couple of chimpanzees sharing an Amazon account? PRIME-mates.'
+,'Why did the teddy bear say no to dessert? Because she was stuffed.'
+,'Why did the soccer player take so long to eat dinner? Because he thought he couldn’t use his hands.'
+,'Name the kind of tree you can hold in your hand? A palm tree!']
+
+
+
+let jokeDisplay = document.getElementById('joke');
+let randomJokes = Math.floor(Math.random('jokes') *8);
+
+joke.innerHTML = ` "${jokes[randomJokes]}"`;
+}
+
+
